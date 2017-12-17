ce8fd3cec1d964e569e17a6f0a709d636dafad6f
SimonGoring <simon.j.goring@gmail.com>
2017-12-16 22:37:28 -0800

    Removed the apoc loading for the neo4j bash files.

--- get_awards.sh
+++ get_awards.sh
@@ -6,7 +6,3 @@ for i in {1959..2017}
 do
   curl -o ./data/input/awards/$i.zip "https://www.nsf.gov/awardsearch/download?DownloadFileName=$i&All=true"
 done
-
-curl -o /home/simon/Downloads/apoc-3.1.3.7-all.jar
-
-cp /apoc-3.1.3.7-all.jar" 
